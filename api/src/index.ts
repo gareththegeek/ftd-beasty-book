169b264adac6af674183d1042e5559253870eb4c
gareththegeek <gj_williams2000@yahoo.co.uk>
2020-12-21 15:15:38 +0000

    Whoops remove report only

--- api/src/index.ts
+++ api/src/index.ts
@@ -16,7 +16,7 @@ app.use((_, res, next) => {
         'Strict-Transport-Security',
         'max-age=31536000; includeSubDomains; preload'
     )
-    res.setHeader('Content-Security-Policy-Report-Only', "default-src 'self'; font-src 'self' https://fonts.gstatic.com; img-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; frame-src 'self';")
+    res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self' https://fonts.gstatic.com; img-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; frame-src 'self';")
     res.setHeader('X-Frame-Options', 'sameorigin')
     res.setHeader('X-Content-Type-Options', 'nosniff')
     res.setHeader('Referrer-Policy', 'no-referrer-when-downgrade')
