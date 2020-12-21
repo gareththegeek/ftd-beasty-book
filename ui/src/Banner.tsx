5a5d746f9717893747ab6135b93c1d0fdf20e5b6
gareththegeek <gj_williams2000@yahoo.co.uk>
2020-12-21 18:43:39 +0000

    Added github link

--- ui/src/Banner.tsx
+++ ui/src/Banner.tsx
@@ -1,11 +1,25 @@
-import { AppBar, Toolbar } from '@material-ui/core'
+import { AppBar, Grid, makeStyles, Toolbar } from '@material-ui/core'
 import React from 'react'
 
+const useStyles = makeStyles(theme => ({
+    icon: {
+        height: theme.spacing(4),
+        width: theme.spacing(4)
+    }
+}))
+
 const Banner: React.FunctionComponent = () => {
+    const classes = useStyles()
+
     return (<header>
         <AppBar position="static">
             <Toolbar>
-                <h2>FTD Beasty Book</h2>
+                <Grid container>
+                    <h2>FTD Beasty Book</h2>
+                </Grid>
+                <Grid container justify="flex-end">
+                    <i title="Contribute and report issues on GitHub!"><a href="https://github.com/gareththegeek/ftd-beasty-book"><img src="./github.png" alt="github repository" className={classes.icon} /></a></i>
+                </Grid>
             </Toolbar>
         </AppBar>
     </header>)
