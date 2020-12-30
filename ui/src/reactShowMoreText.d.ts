ad2bb04b9ffc949d182686c36d34165e153a158f
gareththegeek <gj_williams2000@yahoo.co.uk>
2020-12-30 15:11:03 +0000

    Adjust layout and make description collapsed with a show more button

---
+++ ui/src/reactShowMoreText.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-show-more-text' {
+    class ShowMoreText extends React.Component<{
+        lines?: number
+        more?: string
+        less?: string
+    }> {
+        constructor(props)
+    }
+    export = ShowMoreText
+}
