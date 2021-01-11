5c4f7344faaa3b37e25b54ca494022c6febba6ff
gareththegeek <gj_williams2000@yahoo.co.uk>
2021-01-11 13:59:12 +0000

    Introduce application insights to API

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,12 @@
 require('dotenv').config()
 import app from './server'
 
+const key = process.env.INSTRUMENTATION_KEY
+if (!!key) {
+    console.log(`Starting application insights for ${key}`)
+    const appInsights = require('applicationinsights')
+    appInsights.setup(key).start()
+}
+
 const port = process.env.PORT ?? 8080
 app.listen(port, () => console.log(`Listening on port ${port}`))
