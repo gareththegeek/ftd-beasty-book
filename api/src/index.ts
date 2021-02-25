9afd88a84418c46fd455bcfc1aee287f941b84bd
Gareth Williams <gwilliams@microsoft.com>
2021-02-25 15:19:35 +0000

    Log warning for missing app insights key on server startup

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,6 +6,8 @@ const key = process.env.APPINSIGHTS_INSTRUMENTATIONKEY
 if (!!key) {
     console.info(`Starting application insights for ${key}`)
     appInsights.setup(key).start()
+} else {
+    console.warn('No instrumentation key found')
 }
 
 const port = process.env.PORT ?? 8080
