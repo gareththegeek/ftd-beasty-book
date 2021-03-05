11cc054d6295d169ba00c4699bd94918ee2d2881
gareththegeek <gj_williams2000@yahoo.co.uk>
2021-03-05 13:19:53 +0000

    Reduce menu below sm breakpoint

--- ui/src/Banner.tsx
+++ ui/src/Banner.tsx
@@ -1,4 +1,5 @@
-import { AppBar, Grid, makeStyles, Toolbar } from '@material-ui/core'
+import { useTheme } from '@material-ui/core'
+import { AppBar, Grid, makeStyles, Toolbar, useMediaQuery } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router'
 import { NavLink } from 'react-router-dom'
@@ -8,22 +9,31 @@ const useStyles = makeStyles(theme => ({
         height: theme.spacing(4),
         width: theme.spacing(4)
     },
+    nav: {
+        marginLeft: '24px'
+    },
     navItem: {
         color: '#fff',
         textDecoration: 'none',
-        padding: '16px',
+        padding: '12px 4px 8px 4px',
         cursor: 'pointer',
         fontSize: '16px',
         lineHeight: '32px',
+        display: 'inline-block',
+        textAlign: 'center',
+        minWidth: '80px'
     },
     navItemActive: {
         color: '#fff',
         textDecoration: 'none',
-        padding: '16px',
+        padding: '12px 4px 6px 4px',
         cursor: 'pointer',
         fontSize: '16px',
         lineHeight: '32px',
-        borderBottom: '2px solid #fff'
+        borderBottom: '2px solid #fff',
+        display: 'inline-block',
+        textAlign: 'center',
+        minWidth: '80px'
     }
 }))
 
@@ -31,10 +41,16 @@ const Banner: React.FunctionComponent = () => {
     const classes = useStyles()
     const { pathname } = useLocation()
     const [path, setPath] = useState<string>()
+    const theme = useTheme()
+    const expandMenu = useMediaQuery(theme.breakpoints.up('sm'))
 
     useEffect(() => {
         setPath(pathname)
     }, [pathname, path])
+
+    const active = (path === '/about' ? 'about' : 'monsters')
+    const showAbout = expandMenu || (active !== 'about')
+    const showMonsters = expandMenu || (active !== 'monsters')
 
     return (<header>
         <AppBar position="static">
@@ -49,23 +65,25 @@ const Banner: React.FunctionComponent = () => {
                         <h2>Five Monsters Deep</h2>
                     </Grid>
                     <Grid item>
-                        <nav>
+                        <nav className={classes.nav}>
                             <Grid container direction="row" alignItems="center" spacing={2}>
                                 <Grid item>
-                                    <NavLink
-                                        to="/about"
-                                        className={path === '/about' ? classes.navItemActive : classes.navItem}
-                                        title="What is Five Monsters Deep?">
-                                        About
-                                    </NavLink>
+                                    {showMonsters &&
+                                        <NavLink
+                                            to="/"
+                                            className={active === 'monsters' ? classes.navItemActive : classes.navItem}
+                                            title="Show me the monsters!">
+                                            Monsters
+                                    </NavLink>}
                                 </Grid>
                                 <Grid item>
-                                    <NavLink
-                                        to="/"
-                                        className={path !== '/about' ? classes.navItemActive : classes.navItem}
-                                        title="Show me the monsters!">
-                                        Monsters
-                                    </NavLink>
+                                    {showAbout &&
+                                        <NavLink
+                                            to="/about"
+                                            className={active === 'about' ? classes.navItemActive : classes.navItem}
+                                            title="What is Five Monsters Deep?">
+                                            About
+                                    </NavLink>}
                                 </Grid>
                             </Grid>
                         </nav>
